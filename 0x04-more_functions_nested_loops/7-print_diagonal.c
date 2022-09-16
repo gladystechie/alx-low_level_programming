77792b4cd4dec98e9b0f31e6930c04afecb80c64
Gladys N <gladysintech@gmail.com>
2022-09-15 17:15:03 -0700

    Task 007

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,24 @@
  */
 void print_diagonal(int n)
 {
-	int len, space;
+int len, space;
 
-	if (n > 0)
+if (n > 0)
+{
+	for (len = 0; len < n; len++)
 	{
-		for (len = 0; len < n; len++)
+		for (space = 0; space < len; space++)
 		{
-			for (space = 0; space < len; space++)
-			{
-				putchar(' ');
-			}
-			putchar('\\');
-
-			if (len == (n - 1))
-			{
-				continue;
-			}
-			putchar('\n');
+			putchar(' ');
 		}
+		putchar('\\');
+
+		if (len == (n - 1))
+		{
+			continue;
+		}
+		putchar('\n');
 	}
-	putchar('\n')
+	putchar('\n');
+}
 }
