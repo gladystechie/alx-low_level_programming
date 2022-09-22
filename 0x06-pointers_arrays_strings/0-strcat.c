797809429ff8e409fdfe9e868e331fa81f9f8e6e
Gladys N <gladysintech@gmail.com>
2022-09-22 04:19:14 -0700

    Task 0 corrected

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strcat - concatenate the string pointed to by @src to
+ * _strcat - concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest
- * @dest:String that will be appended
- * @src:String to be concatenated upon
+ * @dest: String that will be appended
+ * @src: String to be concatenated upon
  *
  * Return: returns pointer to @dest
  */
