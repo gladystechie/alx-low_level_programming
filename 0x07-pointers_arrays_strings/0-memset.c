98e21621fef54faa668c714841761730cfd171b8
Gladys N <gladysintech@gmail.com>
2022-09-26 13:43:21 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,11 @@
  * Return: pointer to the memory block
  */
 char *_memset(char *s, char b, unsigned int n)
-	{
-		while (n)
-		{
-			s[n-1]=b;
-			n--;
-		}	
-		return (s);
-	}	
+{
+while (n)
+{
+	s[n-1]=b;
+	n--;
+}	
+return (s);
+}	
