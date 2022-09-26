07856b952755048a69fd913941d3281b15660226
Gladys N <gladysintech@gmail.com>
2022-09-26 15:47:31 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,16 +10,16 @@ char *_strstr(char *haystack, char *needle)
 {
 unsigned int i = 0; j = 0;
 
-while (haystack [i])
+while (haystack[i])
 {
-	while (needle [j] && (haystack [i] == needle [0]))
+	while (needle[j] && (haystack[i] == needle[0]))
 	{
-		if (haystack [i + j] == needle [j])
+		if (haystack[i + j] == needle[j])
 			j++;
 		else
 			break;
 	}
-	if (needle [j])
+	if (needle[j])
 	{
 		i++;
 		j = 0;
