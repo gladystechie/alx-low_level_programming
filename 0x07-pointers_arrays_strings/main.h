c0ce7ae9b18a896d688562848f0530359180121e
Gladys N <gladysintech@gmail.com>
2022-09-26 07:53:11 -0700

    Task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,12 +3,12 @@
 
 /**
  * void prototypes()
- * int prototypes ()
+ * int prototypes()
  */
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char *c);
 unsigned int _strspn(char *s, char *accept);
 char *strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
