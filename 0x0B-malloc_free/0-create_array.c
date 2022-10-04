1ee7b8f96e382e7c27b9a6968e34d70fd7f29da5
Gladys N <gladysintech@gmail.com>
2022-10-04 14:01:10 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,11 @@ unsigned int index;
 
 if (size == 0)
 	return (NULL);
-array = malloc(sizeof(char) *size);
+array = malloc(sizeof(char) * size);
 
 if (array == NULL)
 	return (NULL);
-for (index = 0; index < size; index ++)
+for (index = 0; index < size; index++)
 	array[index] = c;
 
 return (array);
