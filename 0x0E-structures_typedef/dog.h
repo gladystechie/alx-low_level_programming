7fe31b06d6a57af8481234340d0ef942422fa023
Gladys N <gladysintech@gmail.com>
2022-10-11 10:35:48 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
-#ifdef DOG_H_FILE
+#ifndef DOG_H_FILE
 #define DOG_H_FILE
-
 /**
  * struct dog - structure definition of a dog
  * @name: dog name
@@ -14,7 +13,7 @@ typedef struct dog
 	char *owner;
 }dog_t;
 
-void init_dog(struct dog*d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
